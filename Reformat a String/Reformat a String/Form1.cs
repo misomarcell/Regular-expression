38b76b9ce949dd2372c03db5f1f471a9b3062d94
Misó Marcell <misomarcell@gmail.com>
2017-07-08 13:38:23 +0200

    Added 'Reformat a String' form

---
+++ Reformat a String/Reformat a String/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Reformat_a_String
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtOutput.Text = ReformatPhone(txtInput.Text);
+        }
+
+        static string ReformatPhone(string s)
+        {
+            Match m = Regex.Match(s, @"^\(?(\d{3})\)?[\s\-]?(\d{3})\-?(\d{4})$");
+
+            return String.Format("({0}) {1}-{2}",
+                                m.Groups[1],
+                                m.Groups[2],
+                                m.Groups[3]);
+        }
+    }
+}
